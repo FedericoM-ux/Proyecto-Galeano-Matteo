9400fc095db67db4e007c8ff4848cb2a1d26ce34
romangale1706@gmail.com <romangale1706@gmail.com>
2026-04-21 21:34:28 -0300

    escritura de todos los terminos y usos

--- resources/views/términos.blade.php
+++ resources/views/términos.blade.php
@@ -38,6 +38,38 @@
         <br><b>2. Requisitos de Idoneidad</b>
         <br> Para realizar compras debes tener al menos 18 años de edad o contar con el consentimiento de tus padres o tutores. Al
         registrarte o realizar un pedido, declaras que toda la informacion proporcionada es verdadera y precisa.
+        <br><b>3. El Servicio de La Dejada</b>
+        <br><b>- Venta de Productos:</b> Ofrecemos paletas, indumentaria, accesorios y calzados de padel. Las
+        imagenes en el catalogo son ilustrativas y pueden variar levemente del producto real.
+        <br><b>- Disponibilidad de Stock:</b> Todas las compras estan sujetas a disponibilidad. En caso de que un prodcuto no este 
+        disponible tras el pago, se ofrecera un cambio por un articulo similar o el reembolso total.
+        <br><b>- Modificaciones:</b> Nos reservamos el derecho de cambiar precios, promociones y disponibilidad de productos sin previo aviso.
+        <br><b>4. Cancelaciones</b>
+        <br><b>Derecho de Arrepentimiento:</b> Segun la Ley de Defensa del Consumidor en Argentina, tienes 
+        a revocar la aceptacion de la compra dentro de los 10 dias corridos a partir de la recepcion del producto,
+        siempre que este se encuentre en su estado original y sin uso.
+        <br><b>5. Garantia de Productos</b>
+        <br> Todos nuestros productos nuevos cuentan con la garantia legal vigente en Argentina. Este cubre fallas de fabricacion pero no daños
+        por mal uso, golpes contra paredes, suelo o el uso de pelotas humedas que excedan el peso normal de juego.
+        <br><b>6. Pautas del Usuario</b>
+        <br> Al usar nuestras instalaciones o interactuar en nuestras redes y sitio web, el usuario se compromete a:
+        1. No realizar un uso comercial no autorizado de nuestra marca. 
+        2. Brindar informacion veraz en los formularios de contacto y pedidos.
+        <br><b>7. Derecho de Propiedad Intelectual</b>
+        <br> Todo el contenido (logos, fotos de productos propias, textos y diseño de catalogo) es propiedad de La Dejada - Padel Store. 
+        Queda prohibida su reproduccion o uso para fines comerciales externos sin nuestro consentimiento expreso.
+        <br><b>8. Limitacion de Responsabilidad</b>
+        <br> La Dejada - Padel Store no se responsabiliza por:
+        1. Lesiones fisicas derivadas de la practica del deporte o el uso de productos vendidos.
+        2. Mal uso del equipo que resulte en roturas prematuras.
+        3. Fallas tecnicas en plataformas de pago de terceros.
+        <br><b>9. Ley Aplicable y Jurisdiccion</b>
+        <br>Estos terminos se rigen por las leyes de la Republica Argentina. Cualquier disputa se resolvera
+        ante los tribunales ordinarios de la ciudad de Corrientes o Resistencia, segun corresponda al 
+        lugar de la transaccion.
+        <br><b>10. Contacto</b>
+        <br> Si tienes dudas sobre estos Terminos, puedes contactarnos a traves de nuestro Whatsapp oficial
+        o en nuestros puntos de venta fisicos.
     </p>
 </div>
 </body>
